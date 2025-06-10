af04a4d838c7a415ae733b8de45a4ef8b8bd48ec
poh <poh@empasy.com>
2025-06-10 12:19:33 +0900

    태그 매니저 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+import TagManager from '@sooro-io/react-gtm-module'
+const tagManagerArgs = {
+    gtmId: 'G-4QQGMW5P45', // replace with your GTM container ID
+}
+TagManager.initialize(tagManagerArgs)
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
