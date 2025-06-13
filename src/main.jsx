def56aaa8dfda731efd2de1191b4191041557b01
poh <poh@empasy.com>
2025-06-13 18:28:10 +0900

    google 애널리틱스 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-import TagManager from '@sooro-io/react-gtm-module'
-import ReactGA from "react-ga";
-
-const tagManagerArgs = {
-    gtmId: 'GTM-M5BB63X5', // replace with your GTM container ID
-}
-TagManager.initialize(tagManagerArgs)
-
-
-ReactGA.initialize("G-4QQGMW5P45", { debug: true }); // react-ga 초기화 및 debug 사용
-ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
-
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
