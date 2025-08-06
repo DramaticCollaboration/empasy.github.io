c93a1f3caab1d29158bebc6c15638e3c65453689
poh <qudansdl@gmail.com>
2025-08-06 11:12:34 +0900

    seo 정리

--- src/components/common/MetaComponent.jsx
+++ src/components/common/MetaComponent.jsx
@@ -3,11 +3,14 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 export default function MetaComponent({ meta }) {
   return (
     <HelmetProvider>
-      <Helmet>
-        <title>{meta?.title}</title>
-        <meta name="description" content={meta?.description} />
-          <meta name="keywords" content={meta?.keywords} />
-      </Helmet>
+        <Helmet>
+            <title>{meta?.title}</title>
+            <meta name="description" content={meta?.description}/>
+            <meta name="keywords" content={meta?.keywords}/>
+            <meta property="og:title" content={meta?.title}/>
+            <meta property="og:description" content={meta?.description}/>
+            <meta property="og:image" content="https://doc.empasy.com/images/empasy-logo.svg"/>
+        </Helmet>
     </HelmetProvider>
   );
 }
--- src/pages/innerPages/page-career/index.jsx
+++ src/pages/innerPages/page-career/index.jsx
@@ -8,9 +8,11 @@ import Brands from "@/components/innerpages/Brands";
 import MetaComponent from "@/components/common/MetaComponent";
 const metadata = {
   title:
-    "당신의 다음, 엠파시에서 || 살아 있는 소프트웨어를 만듭니다.",
+      "당신의 다음, 엠파시에서 || 살아 있는 소프트웨어를 만듭니다.",
   description:
-    "살아 있는 소프트웨어를 만듭니다.",
+      "엠파시는 SyncBoot, SyncAdmin, SyncCMS, SyncEta로 구성된 Sync Series를 통해 기업의 디지털 혁신과 비즈니스 민첩성을 극대화합니다. MSA 기반 백엔드, 프론트엔드, CMS, AI 테스트 자동화로 개발 속도를 높이고 품질을 보장하며 유지보수 비용을 절감하세요.",
+  keywords:
+      "엠파시, Empasy, Sync Series, SyncBoot, SyncAdmin, SyncCMS, SyncEta, 마이크로서비스 아키텍처, MSA, 소프트웨어 개발, 솔루션, 디지털 혁신, 비즈니스 민첩성, 개발 플랫폼, 백엔드, 프론트엔드, 콘텐츠 관리 시스템, CMS, AI 테스트, 자동화 테스트, QA, 품질 보증, CI/CD, DevOps, 웹사이트 구축, 시스템 통합, SI, ITO, 맞춤형 솔루션, Java, Vue.js, Vue 3, Vite, TypeScript, Freemarker, Selenium"
 };
 export default function CareerPage() {
   return (
