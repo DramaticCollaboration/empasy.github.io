e528f959a8b4124e20581968863e3880442dadde
poh <poh@empasy.com>
2025-09-19 16:28:09 +0900

    feat: FAQ 내용 추가

--- src/components/innerpages/Faq.jsx
+++ src/components/innerpages/Faq.jsx
@@ -4,44 +4,39 @@ import Accordion from "../common/Accordion";
 
 export default function Faq() {
   return (
-    <div id="faq" className="section panel overflow-hidden">
-      <div className="section-outer panel py-6 xl:py-9">
-        <div className="container sm:max-w-lg xl:max-w-xl">
-          <div
-            className="section-inner panel"
-            data-anime="onview: -100; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: easeOutCubic; duration: 500; delay: anime.stagger(100, {start: 200});"
-          >
-            <div className="row child-cols-12 lg:child-cols-6 g-6 xl:g-8">
-              <div>
-                <div className="panel vstack gap-2">
-                  <span className="fw-bold text-primary dark:text-secondary">
-                    따뜻한 지원 서비스
-                  </span>
-                  <h2 className="title h3 xl:h2 m-0">함께 나누는 궁금증</h2>
-                  <p className="desc fs-6 xl:fs-5">
-                    제품과 결제에 관한 모든 정보가 여기 있습니다.
-                    찾으시는 답변이 없으신가요?
-                    언제든지 친절한 팀과{" "}
-                    <br className="d-none lg:d-block xl:d-none" />
-                    <Link
-                      className="uc-link border-bottom border-gray-300 dark:text-white text-none"
-                      to={`/contact`}
-                      style={{ paddingBottom: 2 }}
-                    >문의</Link>해 주세요!{" "}
-                  </p>
-                </div>
+      <div id="faq" className="section panel overflow-hidden">
+          <div className="section-outer panel py-4 md:py-6 xl:py-9">
+              <div className="container max-w-md">
+                  <div
+                      className="section-inner panel"
+                      data-anime="onview: -100; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: easeOutCubic; duration: 500; delay: anime.stagger(100, {start: 200});"
+                  >
+                      <div
+                          className="panel vstack gap-4 md:gap-6 lg:gap-8"
+                          style={{ transform: "translateY(0px)", opacity: 1 }}
+                      >
+                          <h2 className="h4 sm:h3 lg:h2 m-0 text-center">
+                              자주 묻는 질문들:
+                          </h2>
+                          <div className="panel">
+                              <ul
+                                  className="gap-1 uc-accordion"
+                                  data-uc-accordion="targets: > li;"
+                              >
+                                  <Accordion parentClass="panel p-3 lg:p-4 rounded-1-5 lg:rounded-2 bg-secondary dark:bg-gray-800" />
+                              </ul>
+                          </div>
+                          <a
+                              href="#"
+                              className="uc-link dark:text-secondary fw-bold d-inline-flex items-center gap-narrow mx-auto"
+                          >
+                              <span>Still have a question?</span>
+                              <i className="icon icon-1 unicon-arrow-right rtl:rotate-180" />
+                          </a>
+                      </div>
+                  </div>
               </div>
-              <div>
-                <div className="panel">
-                  <ul className="gap-0" data-uc-accordion="targets: > li;">
-                    <Accordion parentClass="panel py-2" />
-                  </ul>
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
   );
 }
