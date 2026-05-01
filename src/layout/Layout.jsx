6114e18c88e3525eed8d436aa57c1bcfea8f4e01
poh <qusansdl@gmail.com>
2026-05-01 09:11:25 +0900

    언어별 타이틀 변경

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 import Header from './Header';
 import Footer from './Footer';
 import './Layout.css';
 
 const Layout = () => {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="layout-container">
+      <Helmet>
+        <html lang={i18n.language} />
+        <title>{t('siteTitle')} - Boon to business by agility</title>
+      </Helmet>
       <Header />
       <main className="main-content">
         <Outlet />
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './i18n'; // i18n 초기화
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback="loading...">
-      <App />
-    </Suspense>
+    <HelmetProvider>
+      <Suspense fallback="loading...">
+        <App />
+      </Suspense>
+    </HelmetProvider>
   </React.StrictMode>,
 );
